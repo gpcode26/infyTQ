f962043b9d1fe3cd8e5d166d6239825fa140ecb6
Guruprasad B R <64017769+gpcode26@users.noreply.github.com>
2020-05-18 12:15:34 +0530

    Add files via upload

---
+++ OOP/OOPR-Assign-3.py
@@ -0,0 +1,13 @@
+class Customer:
+
+    def __init__(self):
+        self.customer_name = None
+        self.bill_amount = None
+
+    def purchases(self):
+        self.bill_amount = self.bill_amount - self.bill_amount * (5 / 100)
+
+    def pays_bill(self, amount):
+        self.bill_amount += amount
+        self.purchases()
+        print(self.customer_name, "pays bill amount of Rs.", self.bill_amount)
